a30fe9991169f2d3a465028c6f409665b33fd931
mickyinhere <micky@arch.nctu.edu.tw>
2021-06-27 19:05:09 +0800

    3dmodel_viewer

---
+++ p53dmodel/sketch.js
@@ -0,0 +1,17 @@
+let octahedron;
+
+function preload() {
+  octahedron = loadModel('3d.obj');
+}
+
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+}
+
+function draw() {
+  clear();
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  model(octahedron);
+}
