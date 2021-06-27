8ec3b879da113411ec427f976efd68bfd2b4086a
mickyinhere <micky@arch.nctu.edu.tw>
2021-06-27 19:47:50 +0800

    u

--- p53dmodel/sketch.js
+++ p53dmodel/sketch.js
@@ -14,10 +14,25 @@ function draw() {
   let locY = mouseY - width / 2;
   ambientLight(60, 60, 60);
   pointLight(255, 255, 255, locX, locY, 100);
+  
+  
+   push();
+  
   rotateX(frameCount * 0.001);
   rotateY(frameCount * 0.001);
-  specularMaterial(250,250,250,155);
+  specularMaterial(160,110,250,255);
   noStroke();
   scale(10);
   model(octahedron);
+  pop();
+ 
+   push();
+
+  rotateX(frameCount * 0.00101);
+  rotateY(frameCount * 0.00101);
+  specularMaterial(250,250,250,155);
+  noStroke();
+  scale(9.5);
+  model(octahedron);
+  pop();
 }
